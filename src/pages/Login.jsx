7461926c4375c10475d03aa846a39a2cf2dcf58c
Do Tran Hieu <dotranhieu925@gmail.com>
2026-04-28 09:12:56 +0700

    The last

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -154,7 +154,7 @@ const Login = () => {
               className="resend-button"
               onClick={() => setIsForgotPasswordOpen(true)}
             >
-              Forgot Password (Check)?
+              Forgot Password?
             </button>
           </div>
 
